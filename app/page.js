96dbeed4cc5d905f9e380a4228f510818d42500a
Olayiwola <olayiwolaajibode@gmail.com>
2024-08-15 16:28:45 -0500

    update

--- app/page.js
+++ app/page.js
@@ -139,46 +139,47 @@ export default function Home() {
               </Stack>
             </Box>
           </Modal>
-          <Button
-            variant="contained"
-            onClick={() => {
-              handleOpen();
-            }}
-
+          <Box bgcolor="grey" spacing={2} justifyContent="center" display="flex" flexDirection="column" borderRadius="15px" overflow="scroll"
           >
-            Add New Item
-          </Button>
-          <Box border="1px solid #333">
-            <Box
-              width="800px"
-              height="100px"
-              bgcolor="#ADD8E6"
-              display="flex"
-              justifyContent="center"
-            >
-              <Typography variant="h2" color="#333">
-                Inventory Items
-              </Typography>
-            </Box>
-          </Box>
-          <Stack width="800px" height="300px" spacing={2} overflow="auto">
-            {inventory.map(({ name, quantity }) => (
-              <Box key={name} width="100%" minHeight="150px" display="flex" alignItems="center" justifyContent="space-between" bgcolor="#f0f0f0" padding={5}>
-                <Typography variant="h3" color="#333" textAlign="center">
-                  {name.charAt(0).toUpperCase() + name.slice(1)}
+            <Button
+              variant="contained"
+              onClick={() => {
+                handleOpen();
+              }}>
+              Add New Item
+            </Button>
+            <Box border="1px solid #333">
+              <Box
+                width="800px"
+                height="100px"
+                bgcolor="#ADD8E6"
+                display="flex"
+                justifyContent="center"
+              >
+                <Typography variant="h2" color="#333" fontFamily="Times New Roman">
+                  Inventory Items
                 </Typography>
-                <Typography variant="h3" color="#333" textAlign="center">
-                  {quantity}
-                </Typography>
-                <Stack direction="row" spacing={2}><Button variant="contained" onClick={() => {
-                  addItem(name);
-                }}>Add</Button>
-                  <Button variant="contained" onClick={() => {
-                    removeItem(name);
-                  }}>Remove</Button></Stack>
               </Box>
-            ))}
-          </Stack>
+            </Box>
+            <Stack width="800px" height="300px" spacing={2} overflow="auto">
+              {inventory.map(({ name, quantity }) => (
+                <Box key={name} width="100%" minHeight="150px" display="flex" alignItems="center" justifyContent="space-between" bgcolor="#f0f0f0" padding={5}>
+                  <Typography variant="h3" color="#333" textAlign="center">
+                    {name.charAt(0).toUpperCase() + name.slice(1)}
+                  </Typography>
+                  <Typography variant="h3" color="#333" textAlign="center">
+                    {quantity}
+                  </Typography>
+                  <Stack direction="row" spacing={2}><Button variant="contained" onClick={() => {
+                    addItem(name);
+                  }}>Add</Button>
+                    <Button variant="contained" onClick={() => {
+                      removeItem(name);
+                    }}>Remove</Button></Stack>
+                </Box>
+              ))}
+            </Stack>
+          </Box>
         </Box>
       </Box>
     </>
